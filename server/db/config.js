983a664fd5e49cf1e4c606e0880e7999bae3138e
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-09-05 18:35:51 -0400

    Setting up for deployment.

--- server/db/config.js
+++ server/db/config.js
@@ -1,12 +1,22 @@
 const { Pool } = require('pg');
-require('dotenv').config({ path: 'src/config/config.env' });
+require('dotenv').config({ path: 'server/config/config.env' });
 
-const pool = new Pool({
+const devConfig = {
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-});
+};
+
+const prodConfig = {
+  connectionString: process.env.DB_URL,
+};
+
+const pool = new Pool(
+  process.env.NODE_ENV === 'production' ? prodConfig : devConfig
+);
+
+console.log(pool);
 
 module.exports = pool;
--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const todoRoutes = require('./routes/todoItems');
 
 const app = express();
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 
 app.use(helmet());
 app.use(express.json());
@@ -21,13 +21,18 @@ app.use('/api/users', userRoutes);
 app.use('/api/todo-lists', todoListRoutes);
 app.use('/api/todo-items', todoRoutes);
 
-// app.use(express.static(path.join(__dirname, 'client/build')));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('/', (req, res) => {
+    app.get('/*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  });
+}
 
-// app.get('/', (req, res) => {
-//   app.get('/*', (req, res) => {
-//     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//   });
-// });
+app.get('*', (req, res) => {
+  res.sendFile(express.static(path.join(__dirname, 'client/build/index.html')));
+});
 
 app.listen(PORT, () => {
   console.log(`Running on port ${PORT}`);
