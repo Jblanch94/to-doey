34ee9be9b5a2c938252995b821558535528a4cb3
Johnny Blanchard <johnnyblanchard94@gmail.com>
2020-09-05 19:45:27 -0400

    Fixed lists reducer.

--- client/src/reducers/listsReducer.js
+++ client/src/reducers/listsReducer.js
@@ -3,7 +3,7 @@ import { FETCH_LISTS } from '../actions/types';
 export default (state = [], action) => {
   switch (action.type) {
     case FETCH_LISTS:
-      return [].concat(state, action.payload);
+      return action.payload;
     default:
       return state;
   }
